60fc237bf9b10ad6c08dcc736ef750edc16c1347
Masum Billah <masumbillah2062003@gmail.com>
2024-11-26 00:08:10 +0600

    first commit

--- src/modules/book_shop_order/order.route.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { orderController } from "./order.controller";
-
-const router = express.Router();
-
-router.put("/", orderController.productOrder);
-router.get("/revenue", orderController.orderRevenueCalculate);
-
-export const prductOrderRouter = router;
